9234a3c22d32f354299a5f6047f02e829a434cc3
Briffon <tarotni33a@gmail.com>
2026-04-02 22:20:12 -0400

    auto push setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Explains to Next.js to build static HTML
+    images: {
+      unoptimized: true, // GitHub Pages doesn't support the Next.js Image Optimizer
+    },
+    // Replace 'your-repo-name' with the actual name of your GitHub repository
+    basePath: '/your-repo-name', 
+    assetPrefix: '/your-repo-name',
+  };
+  
+  export default nextConfig;
