4b9d8f30cc900fa75b74e30e93fad3bebff68aab
Briffon <tarotni33a@gmail.com>
2026-04-02 22:25:18 -0400

    gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Explains to Next.js to build static HTML
-    images: {
-      unoptimized: true, // GitHub Pages doesn't support the Next.js Image Optimizer
-    },
-    // Replace 'your-repo-name' with the actual name of your GitHub repository
-    basePath: '/your-repo-name', 
-    assetPrefix: '/your-repo-name',
+    output: 'export',
+    images: { unoptimized: true },
+    // IMPORTANT: This must match your repo name exactly
+    basePath: '/FadesAndFacials', 
   };
   
   export default nextConfig;
